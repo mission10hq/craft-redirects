de8347017700f013c1b2ea22172efae6ff497503
CameronHay <cameron@mission10.co.uk>
2024-04-01 17:11:49 +0100

    can now redirect to external url

--- src/controllers/RedirectsController.php
+++ src/controllers/RedirectsController.php
@@ -6,35 +6,39 @@ use mission10\redirects\Redirects;
 
 class RedirectsController extends Controller {
 
-    public function actionSave(){
+    public function actionSave() {
         $req = \Craft::$app->getRequest();
         $session = \Craft::$app->getSession();
 
         $from = $req->getRequiredBodyParam(name:'fromURL');
         $to = $req->getRequiredBodyParam(name:'toURL');
         $subpages = $req->getRequiredBodyParam(name:'subpages');
-        
+        $external = $req->getRequiredBodyParam(name:'external');
+
         if(empty($from) || empty($to)){
             $session->setError("You haven't filled required fields");
         }else {
-            $fromResult = $this->getPathFromUrl($from);
-            
-            $from = $subpages == true ? $fromResult['path'] . "($|\/(.*)$|\?.*|\/$|\/\?.*|$)" : $fromResult['path'] . "(\?.*|\/$|\/\?.*|$)";
-            $to = $this->checkToUrl($to);
-            
-            Redirects::$plugin->redirects->save($from,$to);
-            $session->setNotice('Redirect Added');
+
+                $fromResult = $this->getPathFromUrl($from);
+                
+                $from = $subpages == true ? $fromResult['path'] . "($|\/(.*)$|\?.*|\/$|\/\?.*|$)" : $fromResult['path'] . "(\?.*|\/$|\/\?.*|$)";
+                $to = $this->checkToUrl($to, $external);
+                
+                Redirects::$plugin->redirects->save($from,$to);
+                $session->setNotice('Redirect Added');
+
         }
         
 
     }
 
-    private function checkToUrl($to){
-        if($to == "/"){
-            return $to . '$1';
-        }else{
+    private function checkToUrl($to, $external = false){
+        if($external == true){
+            return $to;
+        } else {
             $toResult = $this->getPathFromUrl($to);
-            $to = $toResult['path'] . '$1';
+            $to = $to == '/' ? $to : $toResult['path'];
+            $to = $to . '$1';
             return $to;
         }
     }
